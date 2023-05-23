697d5ad35bdf081fd997f0e4f741d5ba44bc6cc0
Mr-AIP <100208208+Mr-AIP@users.noreply.github.com>
2023-05-23 16:07:15 +0300

    Fresh shitcode

---
+++ emplo/emplo/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emplo
+{
+    public class Employee
+    {
+        private string _fio;
+        private int _departament;
+        private decimal _selary;
+        private int _id;
+        private static int count = 1;
+
+        public Employee(string fio, decimal salary, int departament)
+        {
+            _departament = departament;
+            _selary = salary;
+            _fio = fio;
+            _id = count++;
+
+        }
+
+        public string fio
+        {
+            get => _fio;
+            set => _fio = value;
+
+        }
+
+        public int Departament
+        {
+            get => _departament;
+            set => _departament = value;
+
+        }
+
+        public decimal Salary
+        {
+            get => _selary;
+            set => _selary = value;
+
+        }
+
+
+
+    }
+}
---
+++ emplo/emplo/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using emplo;
+using System.Security.Cryptography.X509Certificates;
+
+
+List<Employee> people = new List<Employee>()
+{
+   new Employee("Moky", 10000, 1),
+   new Employee("Doky", 12200, 2),
+   new Employee("Loky", 12200, 4),
+   new Employee("Shmoky", 15200, 3),
+   new Employee("Miky", 11200, 5),
+   new Employee("Dily", 15200, 4),
+   new Employee("Fill", 16200, 3),
+   new Employee("Miner", 1200, 2),
+   new Employee("Dummy", 15200, 5),
+   new Employee("Fruit", 11200, 1),
+   new Employee("Porry", 11200, 3),
+};
+
+void getInfoAboutPeople()
+{
+    foreach (Employee e in people)
+    {
+        Console.Write(e.fio+" ");
+        Console.Write(e.Salary+" ");
+        Console.Write(e.Departament+"\n");
+        
+    }
+}
+void getSum() 
+{
+    decimal sum = 0;
+    foreach(Employee e in people) 
+    {
+        sum += e.Salary;
+    }
+    Console.WriteLine($"Затраты в месяц: {sum}");
+}
+void getMax() 
+{  
+    
+}
+getInfoAboutPeople();
+getSum();
+
