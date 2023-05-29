8457cf2636f5867362215595c81257f17724d1fd
Mr-AIP <100208208+Mr-AIP@users.noreply.github.com>
2023-05-29 22:35:51 +0300

    Add files via upload

--- emplo/emplo/Program.cs
+++ emplo/emplo/Program.cs
@@ -26,8 +26,14 @@ while (loop)
       "4 - максимальная зарплата\n" +
       "5 - средняя зарплата\n" +
       "6 - фио\n" +
-      "7 - индексация зарплаты \n"+
-      "8 - Выйти");
+      "7 - индексация зарплаты\n" +
+      "8 - информация по отделам\n"+
+      "9 - минимальная зарплата среди сотруднико определенного отдела\n"+
+      "10- максимальная зарплата среди сотруднико определенного отдела\n" +
+      "11- индексация зарплаты по определенному отделу\n"+
+      "12 - вывод информацию о сотрудниках по условию зарплаты меньше числа\n"+
+      "13 - вывод информацию о сотрудниках по условию зарплаты больше или равно числу\n" +
+      "0 - выход");
     try
     {
         switch (Convert.ToInt32(Console.ReadLine()))
@@ -54,16 +60,33 @@ while (loop)
                 break;
             case 6:
                 GetFio();
-                  break;
-                case 7:
+                break;
+            case 7:
                 Console.WriteLine("Ввведите процент индексации");
                 double number = Convert.ToDouble(Console.ReadLine());
                 Endexer(number);
                 break;
-                case 8:
+            case 8:
                 DeprtmentInfo();
                 break;
             case 9:
+                 GetDepartamentSalaryMin();
+                 break;
+                case 10:
+                GetDepartamentSalaryMax();
+                break;
+                case 11:
+                IndexerDept();
+                break;
+                case 12:
+                double num =double.Parse(Console.ReadLine());
+               
+                break;
+            case 13:
+                double n =double.Parse(Console.ReadLine());
+                
+                break;
+            case 0:
                 loop = false;
                 break;
 
@@ -78,7 +101,6 @@ while (loop)
 
     }
 
-    DeprtmentAvgInfo();
     void GetInfoAboutPeople(List<Employee> employee)
     {
         foreach (Employee e in people)
@@ -101,15 +123,15 @@ while (loop)
 
     void GetMax()
     {
-        
+
         Console.WriteLine($"Человек с самой максимальной зарплатой {people.Max(e => e.Salary + " рублей: " + e.Fio)} ");
     }
-     void GetMin()
-    {   
-         people.Min(e => e.Salary + "рублей: " + e.Fio);
-        
+    void GetMin()
+    {
+        people.Min(e => e.Salary + "рублей: " + e.Fio);
+
     }
-    
+
     void Endexer(double num)
     {
 
@@ -157,17 +179,81 @@ while (loop)
     {
 
         Console.WriteLine("Введите номер отдела:");
+        double sum = 0, avg;
+        
         int departmentID = int.Parse(Console.ReadLine());
         foreach (var emp in people)
         {
             if (emp.Department == departmentID)
             {
-                GetAverage();
+                foreach(var e in people)
+                {
+                    sum += e.Salary;
+                }
+                avg= sum /people.Count ;
+                Console.WriteLine(avg);
             }
         }
-        s
+
+    }
+    void GetDepartamentSalaryMin()
+    {
+        Console.WriteLine("Введите номер отдела");
+        double department = double.Parse(Console.ReadLine());
+        var employeesInDepartment = people.Where(e => e.Department == department);
+        if (employeesInDepartment.Any())
+        {
+            var employeeWithMinSalary = employeesInDepartment.OrderBy(e => e.Salary).First();
+            Console.WriteLine($" {employeeWithMinSalary.Fio} - {employeeWithMinSalary.Salary}");
+        }
+        else
+        {
+            Console.WriteLine($"Отдел {department} не найден или не имеет сотрудников.");
+        }
+    }
+    void GetDepartamentSalaryMax()
+    {
+        Console.WriteLine("Введите номер отдела"); double department = double.Parse(Console.ReadLine());
+        var employeesInDepartment = people.Where(e => e.Department == department); if (employeesInDepartment.Any())
+        {
+            var employeeWithMaxSalary = employeesInDepartment.OrderBy(e => e.Salary).Last();
+            Console.WriteLine($" {employeeWithMaxSalary.Fio} - {employeeWithMaxSalary.Salary}");
+        }
+        else
+        {
+            Console.WriteLine($"Отдел {department} не найден или не имеет сотрудников.");
+        }
+
     }
 
+    void IndexerDept()
+    {
+        Console.WriteLine("Введите номер отдела");
+        double department = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите процент индексации зарплат в отделе в формате '1,05'");
+        double indexer = double.Parse(Console.ReadLine());
+        var employeesInDepartment = people.Where(e => e.Department == department);
+        if (employeesInDepartment.Any())
+        {
+            foreach( var e in people)
+            {
+                e.Salary *= indexer;
+            }
+            foreach (var e in people)
+            {
+                Console.WriteLine(e.Salary);
+            }
+
+        }
+        else
+        {
+            Console.WriteLine($"Отдел {department} не найден или не имеет сотрудников.");
+        }
+    }
+
+    
+
+    
 }
 
 
